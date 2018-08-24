a4a6e9d34bf75a11538dfe58bbe19f1d2abedbf0
Arthur Lima Oliveira <=>
2018-08-24 16:57:54 -0300

    Inicio da lógica de negocio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 
 @Component({
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+  
   title = 'Boliche';
+  quadro = [];
+  pinos = 0;
   pontos = 0;
+
+  ngOnInit(): void {
+    
+  }
+
+  jogar(): void{
+
+    this.quadro.push(this.pinos);
+
+  }
+
+  calcularPontos(){
+
+  }
+
 }
+
